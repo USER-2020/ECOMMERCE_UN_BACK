7821503790abf1d30e7aba41e479e3d965443f4a
USER - 2020 <hackingetico2021@gmail.com>
2024-03-01 23:41:14 -0500

    feat: :sparkles: Create base projetc back

---
+++ src/main/java/universidad/ecommerce_universitario_mysql_webflux/ECommerceUniversitarioConMySqlYWebFluxApplication.java
@@ -0,0 +1,13 @@
+package universidad.ecommerce_universitario_mysql_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceUniversitarioConMySqlYWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceUniversitarioConMySqlYWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/universidad/ecommerce_universitario_mysql_webflux/ECommerceUniversitarioConMySqlYWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package universidad.ecommerce_universitario_mysql_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceUniversitarioConMySqlYWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
